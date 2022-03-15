3cefa0927da2728bb5a8c7ed4cb0a4a8b5b2fc1a
Mayank Padhma <232001mp@gmail.com>
2022-03-16 00:12:01 +0530

    initial commit

---
+++ app/src/main/java/com/dev/customerinfo/addcustomer.java
@@ -0,0 +1,14 @@
+package com.dev.customerinfo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class addcustomer extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_addcustomer);
+    }
+}
